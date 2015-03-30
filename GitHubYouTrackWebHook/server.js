f353c7a4f80b4ad0ac73dbaa06d9641b57c0cbff
Luc <hardbarger@infoplanit.com>
2015-03-30 12:34:13 -0500

    This has a function to see if message has issue #abc-123 yup

--- GitHubYouTrackWebHook/server.js
+++ GitHubYouTrackWebHook/server.js
@@ -3,16 +3,36 @@ var port = process.env.port || 1337;
 
 var createHandler = require('github-webhook-handler');
 var handler = createHandler({ path: '/webhook', secret: 'hah' });
+var hasCommitCommand = function (message) {
+	// iss = #abc-123
+	// cmd = fixed assignee luc state testing
+	var re = /(?<iss>\#\w+\-\d+)\s(?<cmd>(\w+)+)/i;
+	return (message.match(re).length > 0);
+};
 
 http.createServer(function (req, res) {
-	handler(req, res, function(err) {
+	handler(req, res, function (err) {
 		res.statusCode = 404;
 		res.end('no such location');
 	});
 }).listen(port);
 
 handler.on('push', function (event) {
+	var payload = event.payload;
+	if (payload == null) {
+		console.error("Payload is null");
+		return;
+	}
+
 	console.log('Received a push event for %s to %s',
-		event.payload.repository.name,
-		event.payload.ref);
-})
+		payload.repository.name,
+		payload.ref);
+	
+	for (var index = 0; index < payload.commits.length; index++) {
+		var commit = payload.commits[index];
+		if (hasCommitCommand(commit.message))
+			console.log("do stuff here with %s", commit.id);
+		else 
+			console.log("nothing to do for %s", commit.id);
+	}
+});
