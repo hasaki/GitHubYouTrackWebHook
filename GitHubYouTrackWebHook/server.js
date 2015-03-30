909c9f3fda77f88edd84c48bd7b1087070efb682
James Johnson <james.johnson@infoplanit.com>
2015-03-30 09:27:53 -0500

    First commit

---
+++ GitHubYouTrackWebHook/server.js
@@ -0,0 +1,18 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({ path: '/webhook', secret: 'hah' });
+
+http.createServer(function (req, res) {
+	handler(req, res, function(err) {
+		res.statusCode = 404;
+		res.end('no such location');
+	});
+}).listen(port);
+
+handler.on('push', function (event) {
+	console.log('Received a push event for %s to %s',
+		event.payload.repository.name,
+		event.payload.ref);
+})
